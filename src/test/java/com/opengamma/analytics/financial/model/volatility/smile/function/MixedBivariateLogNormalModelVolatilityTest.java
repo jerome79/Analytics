fce2c845c6d3677d8ef5fa82813ab2131700be1a
Stuart Archibald <stuart@opengamma.com>
2015-05-07 16:53:44 +0100

    Sweep 7

--- src/test/java/com/opengamma/analytics/financial/model/volatility/smile/function/MixedBivariateLogNormalModelVolatilityTest.java
+++ src/test/java/com/opengamma/analytics/financial/model/volatility/smile/function/MixedBivariateLogNormalModelVolatilityTest.java
@@ -25,7 +25,6 @@ public class MixedBivariateLogNormalModelVolatilityTest {
   /**
    * 
    */
-  @Test
   public void trivialValuesTest() {
     final double[] wghts = {0.5, 0.5 };
     final double[] sigsX = {0.5, 0.5 };
@@ -60,13 +59,11 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     assertEquals(volObjZ2.getPriceZ(option, forwardZ), BlackFormulaRepository.price(forwardZ, k, timeToExpiry, Math.pow(0.5, 0.5), true), EPS);
     assertEquals(volObjZ1.getInvExpDriftCorrection(), 1. / Math.exp(0.25), EPS);
     assertEquals(volObjZ2.getInvExpDriftCorrection(), 1. / Math.exp(0.25), EPS);
-    //    assertEquals(volObjZ2.getSigmasZ()[1], Math.pow(0.5, 0.5), EPS);
   }
 
   /**
    * Parameters for X,Y are also resorted corresponding to sorting of sigmaZ
    */
-  @Test
   public void reorderedDataTest() {
     final double[] wghts = {0.2, 0.8 };
     final double[] sigsX = {0.4, 0.6 };
@@ -627,9 +624,7 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] sigsX = {0.4, 0.6, 0.7 };
     final double[] sigsY = {0.45, 0.55 };
     final double[] rhs = {0., 1. };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rhs);
-
   }
 
   /**
@@ -643,9 +638,7 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] rpfsX = {1., 1. };
     final double[] rpfsY = {1., 1. };
     final double[] rhs = {0., 1. };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rpfsX, rpfsY, rhs);
-
   }
 
   /**
@@ -657,9 +650,7 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] sigsX = {0.4, 0.6 };
     final double[] sigsY = {0.45, 0.55, 0.7 };
     final double[] rhs = {0., 1. };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rhs);
-
   }
 
   /**
@@ -673,9 +664,7 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] rpfsX = {1., 1., 1., };
     final double[] rpfsY = {1., 1. };
     final double[] rhs = {0., 1. };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rpfsX, rpfsY, rhs);
-
   }
 
   /**
@@ -689,7 +678,6 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] rpfsX = {1., 1. };
     final double[] rpfsY = {1., 1., 1. };
     final double[] rhs = {0., 1. };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rpfsX, rpfsY, rhs);
 
   }
@@ -705,7 +693,6 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] rpfsX = {1., 1. };
     final double[] rpfsY = {1., 1. };
     final double[] rhs = {0., 1., 0.1 };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rpfsX, rpfsY, rhs);
 
   }
@@ -719,118 +706,8 @@ public class MixedBivariateLogNormalModelVolatilityTest {
     final double[] sigsX = {0.4, 0.6 };
     final double[] sigsY = {0.45, 0.55 };
     final double[] rhs = {0., 1., 0.1 };
-
     new MixedBivariateLogNormalModelVolatility(wghts, sigsX, sigsY, rhs);
-
   }
 
-  /**
-   * Tests below are for debugging
-   */
-  @Test
-  (enabled = false)
-  public void printTest() {
-
-    //  final double[] constWeights = {0.6, 0.3, 0.1 };
-    //  final double[] sigmasX = {0.1, 0.5, 0.9 };
-    //  final double[] sigmasY = {0.2, 0.7, 1.2 };
-    //  final double[] relativePartialForwardsX = {1., 1., 1. };
-    //  final double[] relativePartialForwardsY = {1., 1., 1. };
-    //  final double[] rhos = {0.4, 0.5, 0.6 };
-
-    //  final double[] constWeights = {0.6, 0.3, 0.1 };
-    //  final double[] sigmasX = {0.3, 0.4, 0.5 };
-    //  final double[] sigmasY = {0.2, 0.6, 0.9 };
-    //  final double[] relativePartialForwardsX = {1., 1., 1. };
-    //  final double[] relativePartialForwardsY = {1., 1., 1. };
-    //  final double[] rhos = {-0.5, 0.5, 0.9 };
-
-    final double[] constWeights = {0.8, 0.2 };
-    final double[] sigmasX = {0.3, 0.4 };
-    final double[] sigmasY = {0.1, 0.2 };
-    final double[] relativePartialForwardsX = {1.1, 0.6 };
-    final double[] relativePartialForwardsY = {1.0, 1.0 };
-    final double[] rhos = {0.1, 0.8 };
-
-    final double[] weights = constWeights;
-
-    final MixedBivariateLogNormalModelVolatility objZ = new MixedBivariateLogNormalModelVolatility(weights, sigmasX,
-        sigmasY, relativePartialForwardsX, relativePartialForwardsY, rhos);
-    final MixedLogNormalModelData objX = new MixedLogNormalModelData(weights, sigmasX, relativePartialForwardsX);
-    final MixedLogNormalModelData objY = new MixedLogNormalModelData(weights, sigmasY, relativePartialForwardsY);
-
-    // final MixedBivariateLogNormalModelVolatility objZ = new MixedBivariateLogNormalModelVolatility(weights, sigmasX,
-    //      sigmasY, rhos);
-    //  final MixedLogNormalModelData objX = new MixedLogNormalModelData(constWeights, sigmasX);
-    //  final MixedLogNormalModelData objY = new MixedLogNormalModelData(constWeights, sigmasY);
-
-    final double forwardX = 1.1;
-    final double forwardY = 0.9;
-    final double forwardZ = forwardX / forwardY;
-    final double timeToExpiry = 0.6;
-    final MixedLogNormalVolatilityFunction volfunc = MixedLogNormalVolatilityFunction.getInstance();
-
-    for (int i = 0; i < 101; i++) {
-      final double k = forwardZ * (0.5 + 1. * i / 100.);
-      final EuropeanVanillaOption option = new EuropeanVanillaOption(k, timeToExpiry, true);
-      final double vol1 = objZ.getImpliedVolatilityZ(option, forwardZ);
-      final double vol2 = volfunc.getVolatility(option, forwardX, objX);
-      final double vol3 = volfunc.getVolatility(option, forwardY, objY);
-      System.out.println(k + "\t" + vol1 + "\t" + vol2 + "\t" + vol3);
-    }
-  }
-
-  /**
-   * 
-   */
-  @Test
-  (enabled = false)
-  public void printTestStrikeTime() {
-
-    //  final double[] constWeights = {0.6, 0.3, 0.1 };
-    //  final double[] sigmasX = {0.1, 0.5, 0.9 };
-    //  final double[] sigmasY = {0.2, 0.7, 1.2 };
-    //  final double[] relativePartialForwardsX = {1., 1., 1. };
-    //  final double[] relativePartialForwardsY = {1., 1., 1. };
-    //  final double[] rhos = {0.4, 0.5, 0.6 };
-
-    //  final double[] constWeights = {0.6, 0.3, 0.1 };
-    //  final double[] sigmasX = {0.3, 0.4, 0.5 };
-    //  final double[] sigmasY = {0.2, 0.6, 0.9 };
-    //  final double[] relativePartialForwardsX = {1., 1., 1. };
-    //  final double[] relativePartialForwardsY = {1., 1., 1. };
-    //  final double[] rhos = {-0.5, 0.5, 0.9 };
-
-    final double[] constWeights = {0.8, 0.2 };
-    final double[] sigmasX = {0.3, 0.4 };
-    final double[] sigmasY = {0.1, 0.2 };
-    final double[] relativePartialForwardsX = {1.1, 0.6 };
-    final double[] relativePartialForwardsY = {1.0, 1.0 };
-    final double[] rhos = {0.1, 0.8 };
-
-    final double[] weights = constWeights;
-
-    final MixedBivariateLogNormalModelVolatility objZ = new MixedBivariateLogNormalModelVolatility(weights, sigmasX,
-        sigmasY, relativePartialForwardsX, relativePartialForwardsY, rhos);
-
-    // final MixedBivariateLogNormalModelVolatility objZ = new MixedBivariateLogNormalModelVolatility(weights, sigmasX,
-    //      sigmasY, rhos);
-    //  final MixedLogNormalModelData objX = new MixedLogNormalModelData(constWeights, sigmasX);
-    //  final MixedLogNormalModelData objY = new MixedLogNormalModelData(constWeights, sigmasY);
-
-    final double forwardX = 1.1;
-    final double forwardY = 0.9;
-    final double forwardZ = forwardX / forwardY;
-
-    for (int j = 0; j < 51; j++) {
-      final double time = 0.5 * (0.5 + 2. * j / 100.);
-      for (int i = 0; i < 51; i++) {
-        final double k = forwardZ * (0.5 + 1. * i / 100.);
-        final EuropeanVanillaOption option = new EuropeanVanillaOption(k, time, true);
-        final double vol1 = objZ.getImpliedVolatilityZ(option, forwardZ);
-        System.out.println(k + "\t" + time + "\t" + vol1);
-      }
-    }
-  }
-
+ 
 }
