d40dec17f782b76754c71f9e734dd87df8b4840c
Stuart Archibald <stuart@opengamma.com>
2015-06-02 18:05:23 +0100

    Clean up DeltaGammaCovarianceMatrixFisherKurtosisCalculator

--- src/main/java/com/opengamma/analytics/financial/var/parametric/DeltaGammaCovarianceMatrixFisherKurtosisCalculator.java
+++ src/main/java/com/opengamma/analytics/financial/var/parametric/DeltaGammaCovarianceMatrixFisherKurtosisCalculator.java
@@ -6,9 +6,9 @@
 package com.opengamma.analytics.financial.var.parametric;
 
 import java.util.Map;
-import java.util.Objects;
 
 import com.opengamma.analytics.math.function.Function1D;
+import com.opengamma.analytics.math.matrix.CommonsMatrixAlgebra;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
 import com.opengamma.analytics.math.matrix.Matrix;
@@ -19,12 +19,10 @@ import com.opengamma.strata.collect.ArgChecker;
  * 
  */
 public class DeltaGammaCovarianceMatrixFisherKurtosisCalculator extends Function1D<Map<Integer, ParametricVaRDataBundle>, Double> {
-  private final MatrixAlgebra _algebra;
+  private static MatrixAlgebra s_algebra = new CommonsMatrixAlgebra();
   private final Function1D<Map<Integer, ParametricVaRDataBundle>, Double> _std;
 
-  public DeltaGammaCovarianceMatrixFisherKurtosisCalculator(final MatrixAlgebra algebra) {
-    ArgChecker.notNull(algebra, "algebra");
-    _algebra = algebra;
+  public DeltaGammaCovarianceMatrixFisherKurtosisCalculator() {
     _std = new DeltaGammaCovarianceMatrixStandardDeviationCalculator();
   }
 
@@ -47,35 +45,12 @@ public class DeltaGammaCovarianceMatrixFisherKurtosisCalculator extends Function
     if (gammaMatrix.getNumberOfColumns() != deltaCovariance.getNumberOfColumns()) {
       throw new IllegalArgumentException("Gamma matrix and covariance matrix were incompatible sizes");
     }
-    final Matrix<?> product = _algebra.multiply(gammaMatrix, deltaCovariance);
+    final Matrix<?> product = s_algebra.multiply(gammaMatrix, deltaCovariance);
     final double std = _std.evaluate(data);
-    final double numerator = _algebra.getTrace(_algebra.getPower(product, 4)) + 12
-        * _algebra.getInnerProduct(delta, _algebra.multiply(_algebra.multiply(deltaCovariance, _algebra.getPower(product, 2)), delta)) + 3 * std * std;
-    final double denominator = Math.pow(0.5 * _algebra.getTrace(_algebra.getPower(product, 2)) + _algebra.getInnerProduct(delta, _algebra.multiply(deltaCovariance, delta)), 2);
+    final double numerator = s_algebra.getTrace(s_algebra.getPower(product, 4)) + 12
+        * s_algebra.getInnerProduct(delta, ((DoubleMatrix2D)s_algebra.multiply(s_algebra.multiply(deltaCovariance, s_algebra.getPower(product, 2)), delta)).getColumnVector(0)) + 3 * std * std;
+    final double denominator = Math.pow(0.5 * s_algebra.getTrace(s_algebra.getPower(product, 2)) + s_algebra.getInnerProduct(delta, ((DoubleMatrix2D)s_algebra.multiply(deltaCovariance, delta)).getColumnVector(0)), 2);
     return numerator / denominator - 3;
   }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + _algebra.hashCode();
-    result = prime * result + _std.hashCode();
-    return result;
-  }
-
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final DeltaGammaCovarianceMatrixFisherKurtosisCalculator other = (DeltaGammaCovarianceMatrixFisherKurtosisCalculator) obj;
-    return Objects.equals(_algebra, other._algebra) && Objects.equals(_std, other._std);
-  }
 }
--- src/test/java/com/opengamma/analytics/financial/var/parametric/DeltaGammaCovarianceMatrixFisherKurtosisCalculatorTest.java
+++ src/test/java/com/opengamma/analytics/financial/var/parametric/DeltaGammaCovarianceMatrixFisherKurtosisCalculatorTest.java
@@ -6,33 +6,25 @@
 package com.opengamma.analytics.financial.var.parametric;
 
 import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertFalse;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.testng.annotations.Test;
 
-import com.opengamma.analytics.math.matrix.ColtMatrixAlgebra;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
-import com.opengamma.analytics.math.matrix.MatrixAlgebra;
 
 /**
  * Test.
  */
 @Test
 public class DeltaGammaCovarianceMatrixFisherKurtosisCalculatorTest {
-  private static final MatrixAlgebra ALGEBRA = new ColtMatrixAlgebra();
-  private static final DeltaGammaCovarianceMatrixFisherKurtosisCalculator F = new DeltaGammaCovarianceMatrixFisherKurtosisCalculator(ALGEBRA);
+  private static final DeltaGammaCovarianceMatrixFisherKurtosisCalculator F = new DeltaGammaCovarianceMatrixFisherKurtosisCalculator();
   private static final DoubleMatrix1D DELTA_VECTOR = new DoubleMatrix1D(new double[] {1, 5 });
   private static final DoubleMatrix2D GAMMA_MATRIX = new DoubleMatrix2D(new double[][] {new double[] {25, -7.5 }, new double[] {-7.5, 125 } });
   private static final DoubleMatrix2D COVARIANCE_MATRIX = new DoubleMatrix2D(new double[][] {new double[] {0.0036, -0.0006 }, new double[] {-0.0006, 0.0016 } });
 
-  @Test(expectedExceptions = IllegalArgumentException.class)
-  public void testNullAlgebra() {
-    new DeltaGammaCovarianceMatrixFisherKurtosisCalculator(null);
-  }
 
   @Test(expectedExceptions = IllegalArgumentException.class)
   public void testNullData() {
@@ -48,15 +40,6 @@ public class DeltaGammaCovarianceMatrixFisherKurtosisCalculatorTest {
     m.put(1, delta);
     m.put(2, gamma);
     F.evaluate(m);
-  }
-
-  @Test
-  public void testEqualsAndHashCode() {
-    final DeltaGammaCovarianceMatrixFisherKurtosisCalculator f1 = new DeltaGammaCovarianceMatrixFisherKurtosisCalculator(ALGEBRA);
-    final DeltaGammaCovarianceMatrixFisherKurtosisCalculator f2 = new DeltaGammaCovarianceMatrixFisherKurtosisCalculator(new ColtMatrixAlgebra());
-    assertEquals(F, f1);
-    assertEquals(F.hashCode(), f1.hashCode());
-    assertFalse(f1.equals(f2));
   }
 
   @Test
