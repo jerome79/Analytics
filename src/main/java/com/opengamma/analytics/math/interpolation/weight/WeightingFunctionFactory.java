a1c846707182a02c9e3eec4ec73994654cc0e60a
Stephen Colebourne <stephen@opengamma.com>
2015-07-06 14:58:13 +0100

    Remove cosine weighting function

--- src/main/java/com/opengamma/analytics/math/interpolation/weight/WeightingFunctionFactory.java
+++ src/main/java/com/opengamma/analytics/math/interpolation/weight/WeightingFunctionFactory.java
@@ -18,8 +18,6 @@ public final class WeightingFunctionFactory {
   public static final String SINE_WEIGHTING_FUNCTION_NAME = "SineWeightingFunction";
   /** Linear weighting function name */
   public static final String LINEAR_WEIGHTING_FUNCTION_NAME = "LinearWeightingFunction";
-  /** Cosine weighting function */
-  public static final CosineWeightingFunction COSINE_WEIGHTING_FUNCTION = CosineWeightingFunction.getInstance();
   /** Sine weighting function */
   public static final SineWeightingFunction SINE_WEIGHTING_FUNCTION = SineWeightingFunction.getInstance();
   /** Linear weighting function */
@@ -30,8 +28,6 @@ public final class WeightingFunctionFactory {
   static {
     final Map<String, WeightingFunction> staticInstances = new HashMap<>();
     final Map<Class<?>, String> instanceNames = new HashMap<>();
-    staticInstances.put(COSINE_WEIGHTING_FUNCTION_NAME, COSINE_WEIGHTING_FUNCTION);
-    instanceNames.put(CosineWeightingFunction.class, COSINE_WEIGHTING_FUNCTION_NAME);
     staticInstances.put(LINEAR_WEIGHTING_FUNCTION_NAME, LINEAR_WEIGHTING_FUNCTION);
     instanceNames.put(LinearWeightingFunction.class, LINEAR_WEIGHTING_FUNCTION_NAME);
     staticInstances.put(SINE_WEIGHTING_FUNCTION_NAME, SINE_WEIGHTING_FUNCTION);
